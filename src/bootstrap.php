4c63ef6481fec5510a51d25ff7311e1493c39521
Robert San <robertsanseries@gmail.com>
2020-05-11 22:49:32 -0300

    parcial commit

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -12,25 +12,3 @@ use Tupi;
 */
 
 require_once __DIR__.'/../src/Common.php';
-
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| Create a new application instance.
-|
-*/
-
-$app = new Tupi\Application(APP_PATH);
-
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-|
-| This script returns the application instance.
-|
-*/
-
-return $app;
